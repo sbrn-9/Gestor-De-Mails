2cbd42ceebca4fa5a114837b47737f1c57d3d6b4
Sabrina <113538071+sbrn-9@users.noreply.github.com>
2023-07-06 11:53:54 -0300

    Delete WebMail/WebMail directory

--- WebMail/WebMail/MailBussines/MailBussines.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MailBussinesInt
-{
-    public class MailBussines
-    {
-
-    }
-}
--- WebMail/WebMail/WebMail/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebMail.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
